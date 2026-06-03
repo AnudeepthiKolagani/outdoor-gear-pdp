ea226609b39a9d91f112cd22617c79c3a5403c0d
Anudeepthi <anudeepthi.kolagani@gmail.com>
2026-06-03 23:20:26 +0530

    Used the colors from _variables.scss file

--- src/pages/ProductDetailPage/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage/ProductDetailPage.tsx
@@ -3,10 +3,14 @@ import { ProductGallery } from "../../components/ProductGallery/ProductGallery";
 import { ProductInfo } from "../../components/ProductInfo/ProductInfo";
 import { ProductDetails } from "../../components/ProductDetails/ProductDetails";
 import styles from "./ProductDetailPage.module.scss";
+import { ShoppingCartIcon } from "lucide-react";
 
 export const ProductDetailPage = (): JSX.Element => {
   return (
     <div className={styles.productPageLayout}>
+      <div className={styles.header}>
+        <ShoppingCartIcon  className={styles.shoppingCartIcon}/>
+      </div>
       <div className={styles.productLayout}>
         <ProductGallery />
         <ProductInfo />
