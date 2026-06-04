00117c4e9f38f9fde7739715b9962efb0df7f600
Anudeepthi <anudeepthi.kolagani@gmail.com>
2026-06-04 19:37:49 +0530

    Written unit test cases for ProductsGallery and ColorSelector components

---
+++ src/test/ProductGallery.test.tsx
@@ -0,0 +1,64 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { ProductGallery } from "../components/ProductGallery/ProductGallery";
+import { describe, expect, it, vi } from "vitest";
+
+type ZoomState = {
+  x: number;
+  y: number;
+  active: boolean;
+};
+const zoomData: ZoomState = {
+  x: 1,
+  y: 1,
+  active: false,
+};
+
+const setZoom = vi.fn();
+
+describe("Products Gallery", () => {
+  it("Should render thumbnail images as heroImage", () => {
+    render(
+      <ProductGallery
+        productImage="./hero.png"
+        zoom={zoomData}
+        setZoom={setZoom}
+      />,
+    );
+
+    const heroImage = screen.getByTestId("hero-image");
+
+    expect(heroImage).toBeInTheDocument();
+  });
+
+  it("Should deactivates zoom on mouse leave", () => {
+    render(
+      <ProductGallery
+        productImage="./hero.png"
+        zoom={zoomData}
+        setZoom={setZoom}
+      />,
+    );
+
+    fireEvent.mouseLeave(screen.getByTestId("hero-image"));
+
+    expect(setZoom).toHaveBeenCalledWith({
+      x: 1,
+      y: 1,
+      active: false,
+    });
+  });
+
+  it("Should render thumbnail images ", () => {
+    render(
+      <ProductGallery
+        productImage="./hero.png"
+        zoom={zoomData}
+        setZoom={setZoom}
+      />,
+    );
+
+    const thumbnails = screen.getAllByRole("img");
+
+    expect(thumbnails.length).toBe(6);
+  });
+});
